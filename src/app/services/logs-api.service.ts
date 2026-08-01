c050fcf20a357c86430e09444545679a6807b8a9
Shubham Haldankar <snh16041998@gmail.com>
2026-08-02 01:12:30 +0530

    Update environment

--- src/app/services/logs-api.service.ts
+++ src/app/services/logs-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchDto } from '../models/search-dto';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { SearchResultDto } from '../models/search-result-dto';
 
 @Injectable({
